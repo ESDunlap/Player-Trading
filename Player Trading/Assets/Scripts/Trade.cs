596408f8e02eaa1ba7b6d0bd8d56eb531725c510
ESDunlap <dunlape3@my.erau.edu>
2025-11-17 10:56:10 -0700

    Finished UI layout

---
+++ Player Trading/Assets/Scripts/Trade.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class Trade : MonoBehaviour
+{
+    public GameObject tradeCanvas;
+    public TextMeshProUGUI inventoryText;
+
+    [HideInInspector]
+    public List<ItemInstance> inventory;
+    [HideInInspector]
+    public List<CatalogItem> catalog;
+
+    public static Trade instance;
+    void Awake() { instance = this; }
+
+    public UnityEvent onRefreshUI;
+
+    public void OnLoggedIn()
+    {
+        tradeCanvas.SetActive(true);
+        if (onRefreshUI != null)
+            onRefreshUI.Invoke();
+    }
+
+    public void GetInventory()
+    {
+
+    }
+
+    public void GetCatalog()
+    {
+        inventoryText.text = "";
+
+        GetPlayerCombinedInfoRequest getInvRequest = new GetPlayerCombinedInfoRequest
+        {
+            PlayFabId = LoginRegister.instance.playFabId,
+            InfoRequestParameters = new GetPlayerCombinedInfoRequestParams
+            {
+                GetUserInventory = true
+            }
+        };
+        PlayFabClientAPI.GetPlayerCombinedInfo(getInvRequest,
+            result =>
+            {
+                inventory = result.InfoResultPayload.UserInventory;
+
+                foreach (ItemInstance item in inventory)
+                    inventoryText.text += item.DisplayName + ", ";
+            },
+            error => Debug.Log(error.ErrorMessage)
+        );
+    }
+}
